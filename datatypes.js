a22247f88d6f3f859c512423910f33bee2438085
IshworKhadka <ishworfloyd@gmail.com>
2024-09-19 22:07:31 +0930

    datatypes

---
+++ datatypes.js
@@ -0,0 +1,47 @@
+
+
+// string
+
+            // single quote
+            // double quote
+            // back tick ---- for template string
+
+            // '......', "........", `......`
+
+            let name = 'John'
+            console.log('Hello ' + name + ',  \
+                Good Afternoon')        // backslash can suppress the error, two tabs in between name and Good in this case if executed
+
+           console.log(`Hello ${name}, 
+   Good Afternoon`)
+
+
+// number
+
+
+// boolean
+
+// null
+            //empty value
+            //have a variable but wants it to be empty, then assign it with null
+
+// undefined
+            // In JS, datatypes are defiend as per value. So, if value is not set, no datatype or undefined
+
+            let x
+            console.log(x)   //output -> undefined
+            console.log(typeof x)       //output -> undefined
+
+            let y = 10
+            console.log(typeof y)       //output -> number
+
+            let z = '10'
+            console.log(typeof z)     //output -> string
+
+            //in JS, three types can have empty value, undefined, null, and empty string
+
+
+// array
+
+
+// object 
